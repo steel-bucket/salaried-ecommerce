c69bdb0804def17d3708c410fea2322c80d5e5ef
steel-bucket <depnra1@gmail.com>
2024-08-19 21:28:12 +0530

    auth finished

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,12 @@
-'use client'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Link from 'next/link'
 import { Icons } from '@/components/Icons'
 import NavItems from './NavItems'
-import { useTheme } from 'next-themes'
-import { buttonVariants } from '@/components/ui/button'
 import Cart from '@/components/Cart'
 import NavAuth from './NavAuth'
+import NavThemeButton from './NavThemeButtton'
 
 const Navbar = () => {
-    const { theme, setTheme } = useTheme()
 
     return (
         <div className="bg-white dark:bg-black sticky z-50 top-0 inset-x-0 h-16">
@@ -27,48 +24,17 @@ const Navbar = () => {
                                     <Icons.logo className="h-10 w-10" />
                                 </Link>
                             </div>
-                            {/**/}
-                            {/**/}
-                            {/**/}
-                            {/**/}
-                            {/**/}
                             <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                                 <NavItems />
                             </div>
-                            {/**/}
-                            {/**/}
-                            {/**/}
-                            {/**/}
-                            {/**/}
-
                             <div className="ml-auto flex items-center lg:space-x-4">
-                                <button
-                                    onClick={() => {
-                                        setTheme(
-                                            theme === 'dark' ? 'light' : 'dark'
-                                        )
-                                    }}
-                                >
-                                    <Icons.moon className="h-10 w-10" />
-                                </button>
-                                {/**/}
-                                {/**/}
-                                {/**/}
-                                {/**/}
-                                {/**/}
-
+                                <NavThemeButton />
                                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-4">
                                     <NavAuth />
                                     <div className="ml-4 flow-root lg:ml-6">
                                         <Cart />
                                     </div>
                                 </div>
-                                {/**/}
-                                {/**/}
-                                {/**/}
-                                {/**/}
-                                {/**/}
-
                             </div>
                         </div>
                     </div>
