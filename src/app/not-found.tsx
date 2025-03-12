e47eca754789c6a273b782d55bf9ec048c2ac5d0
Deepnarayan Sett <depnra1@gmail.com>
2025-03-12 12:00:32 +0530

    Analytics Platform - Debugging 8

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,6 +7,9 @@ export default function NotFound() {
             <div className="mx-auto max-w-fit h-full mt-80">
                 <h1 className="font-semibold  text-4xl">
                     I&apos;m sorry, that page doesn&apos;t exist</h1>
+                <p className="text-lg mt-4">
+                    Please contact the developer if you think this is a mistake
+                </p>
                 <div className="items-center mx-auto max-w-fit mt-8">
                     <Link href="/">Home</Link>
                 </div>
